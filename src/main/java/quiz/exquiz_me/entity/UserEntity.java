9e94a40f8b78391c123c5c918f4827b4f5243d73
jeongeun lee <lje7370@gmail,com>
2024-04-15 21:07:33 +0900

    third commit

--- src/main/java/quiz/exquiz_me/entity/UserEntity.java
+++ src/main/java/quiz/exquiz_me/entity/UserEntity.java
@@ -1,45 +1,26 @@
 package quiz.exquiz_me.entity;
 
-
-import jakarta.persistence.*;
-import lombok.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 @Getter
 @Setter
-@Table(name = "user")
 public class UserEntity {
+        @Id
+        private String email;
+        private String password;
+        private String nickname;
+        private String telNumber;
+        private LocalDate date;
+        private Integer gender;
+        private String signupPurpose;
+        private String identity;
+        private String oneLineResolution;
 
-    @Id
-    private String email;
-    private String password;
-
-
-
-    @Column(unique = true)
-    private String nickname;
-
-    @Column(name = "tel_number", unique = true)
-    private String telNumber;
-
-    private LocalDate date; // 사용자 생년월일
-
-    private Integer gender;
-
-    @Column(name = "signup_purpose")
-    private String signupPurpose;
-
-    private String identity;
-
-    @Column(name = "one_line_resolution")
-    private String oneLineResolution;
-
-    private String permission;
+        private String permission;
 }
