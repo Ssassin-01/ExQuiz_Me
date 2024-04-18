0376886892d9d0f476e57d28619e997626ec646e
Ssassin-01 <71809583+Ssassin-01@users.noreply.github.com>
2024-04-18 09:24:12 +0900

    version check

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HelmetProvider } from 'react-helmet-async';
 import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
 import Home from "./components/Home";
@@ -10,15 +11,19 @@ import SignUp from "./components/SignUp";
 import "./App.css";
 import StudySection from "./components/StudySection";
 import MakeComponent from "./components/MakeComponent";
+import MyComponent from './components/MyComponent';
 
 function App() {
 
 
     return (
         <>
-
+            <HelmetProvider>
+                <div>
+                    <MyComponent />
+                </div>
+            </HelmetProvider>
             <Router>
-
                 <div className="app">
 
                     <div className="content">
